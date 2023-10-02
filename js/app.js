36d248f705eb382cb2551047f2d17ae50a6c560f
Brad Smith <bradsmith@duck.com>
2023-10-02 17:09:51 -0400

    Add #1 (filter)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
-const docTitle = document.querySelector('body h1');
+/*******************************
+/ just my stuff.not class-related
+/******************************/
+const docTitle = document.querySelector('#pageTitle');
 
-console.log(`My ${docTitle.innerText.toLowerCase()}/app.js is being run…`);
+console.log(`My ${docTitle.innerText.toLowerCase()}/app.js is available…`);
 const titlePEl = document.createElement('p');
-titlePEl.innerHTML = `My <strong>${docTitle.innerText.toLowerCase()}/app.js</strong> is being run…`;
+titlePEl.innerHTML = `My <strong>${docTitle.innerText.toLowerCase()}/app.js</strong> is available…`;
 document.querySelector('body').appendChild(titlePEl);
 
+
+/*******************************
+/ just my stuff.not class-related
+/******************************/
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+// 1. filter()
+const oldies = inventors.filter(inventor => inventor.year < 1600 && inventor.year > 1499);
+console.log(oldies);
