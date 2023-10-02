44a1ed99ca07cbf312566ae2c8d412abc88fc711
Brad Smith <bradsmith@duck.com>
2023-10-02 17:16:31 -0400

    Add #4 (find)

--- js/app.js
+++ js/app.js
@@ -28,6 +28,93 @@ const inventors = [
   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
-// 1. filter()
+// Array.prototype.filter()
+// 1. Filter the array of inventors into a new array containing only the inventors born in the 1500's
 const oldies = inventors.filter(inventor => inventor.year < 1600 && inventor.year > 1499);
 console.log(oldies);
+
+
+// Array.prototype.map()
+// 2. Map the array of the inventors into a new array containing objects with just the first and last names as properties
+// Hint:  Return a new object literal from the callback (don't mutate the object being passed in to map)
+const namesOnly = inventors.map(person => 
+  console.log(person)
+);
+// console.log(namesOnly);
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birth date (year property), in ascending order
+
+
+// Array.prototype.find()
+// 4. Find the inventor object with the first name of 'Ada'
+const theAda = inventors.find(person => person.first.includes('Ada'));
+console.log(theAda);
+
+
+// Array.prototype.reduce()
+// 5. How many years did all the inventors live?
+
+
+const people = [
+  'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
+  'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
+  'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
+  'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
+  'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
+  'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
+  'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
+  'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
+  'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
+  'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
+  'Blake, William'
+];
+
+// Array.prototype.map()
+// 6. Map the people array such that the new array consists of strings with the names formatted as "First Last", e.g., "Becker, Carl" should be mapped to "Carl Becker".
+// Hint: As a start, consider using the String.prototype.split method to "split" the string using ', ' as the separator
+
+
+
+const data = [
+  'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
+  'bike', 'walk', 'car', 'van', 'car', 'truck'
+];
+
+// Array.prototype.reduce()
+// 7. Count the number of instances for each of the data items. The reduce should return an object where the keys are 'car', 'truck', etc. and the values are the count.
+// Hint: Since you want to return an object, be sure to pass an empty {} for the initial value of the "accumulator".
+
+
+
+const devs = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+// Array.prototype.some()
+// 8. Check if at least one person is 19 or older?
+// Hint: To get today's year, use the getFullYear method of new Date(), i.e., new Date().getFullYear()
+
+
+// Array.prototype.every()
+// 9. Check if everyone is 19 or older?
+
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Array.prototype.find()
+// 10. Find the comment with the id of 823423
+
+
+
+// Array.prototype.findIndex()
+// 11. Find the index of the comment with an id of 123523
